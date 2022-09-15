55c71418b613df22700c6326bb4cacf0ee0d2164
Kader1680 <ouldhenniabaghdad@gmail.com>
2022-09-15 19:42:37 +0100

    code home

--- src/home/home.js
+++ src/home/home.js
@@ -8,6 +8,12 @@ import brand5 from '../images/brand-05.png'
 import brand6 from '../images/brand-06.png'
 import brand7 from '../images/brand-07.png'
 import brand8 from '../images/brand-08.png'
+import Cont from '../images/contact-image.jpg'
+import t1 from '../images/team-dark-01.jpg'
+import t2 from '../images/team-dark-02.jpg'
+import t3 from '../images/team-dark-03.jpg'
+import t4 from '../images/team-dark-04.jpg'
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -35,18 +41,90 @@ export default class Footer extends React.Component {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quis. Ullam accusantium dignissimos repellendus nemo fugiat numquam, nisi odio adipisci. Veniam neque itaque expedita officiis rem ipsa! Ratione, rem reiciendis?
                     </div>
                 </div>
-                <div class="feature row bg-danger">
-                        
-                        <img src={brand1} class="row col-xs-4" />
-                        <img src={brand2} class="row col-xs-4" />
-                        <img src={brand3} class="row col-xs-4" />
-                        <img src={brand4} class="row col-xs-4" />
-                        <img src={brand5} class="row col-xs-4" />
-                        <img src={brand6} class="row col-xs-4" />
-                        <img src={brand7} class="row col-xs-4" />
-                        <img src={brand8} class="row col-xs-4" />
+                <div class="feature row ">
+                    <img src={brand1} class="row col-xs-4" />
+                    <img src={brand2} class="row col-xs-4" />
+                    <img src={brand3} class="row col-xs-4" />
+                    <img src={brand4} class="row col-xs-4" />
+                    <img src={brand5} class="row col-xs-4" />
+                    <img src={brand6} class="row col-xs-4" />
+                    <img src={brand7} class="row col-xs-4" />
+                    <img src={brand8} class="row col-xs-4" />
 
+                </div>
+
+
+
+
+                {/* BLOG PUBLICATION */}
+
+                <div class="row publication">
+                    <div class="row col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        <img src={Cont}  />
                     </div>
+                    <div class="row col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        <h2>Doob specializes in small Corporate Business.</h2>
+                        <div class="chart text-white mb-sm-5">
+                            <span class="done p-2 "><i class="fa-solid fa-check"></i></span>
+                            <span class="ms-3 fs-5 fw-bold">A good traveler has no fixed plans</span>
+                            <div class="ms-5">Lorem ipsum dolor consectetur adipiscing do eiusmod tempor incididunt labore.</div>
+                        </div>
+                        <div class="chart text-white">
+                            <span class="done p-2"><i class="fa-solid fa-check"></i></span>
+                            <span class="ms-3 fs-5 fw-bold">A good traveler has no fixed plans</span>
+                            <div class="ms-5">Lorem ipsum dolor consectetur adipiscing do eiusmod tempor incididunt labore.</div>
+                        </div>
+                        <button class="w-50">About Our Doop</button>
+                    </div>
+                </div>
+
+
+
+
+
+                {/* BLOG NUMBER */}
+
+                <div class="number row text-center">
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <span>+199</span>
+                        <div class="text-white">Happy Clients</div>
+                    </div>
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <span>+199</span>
+                        <div class="text-white">Happy Clients</div>
+                    </div>
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <span>+199</span>
+                        <div class="text-white">Happy Clients</div>
+                    </div>
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <span>+199</span>
+                        <div class="text-white">Happy Clients</div>
+                    </div>
+                </div>
+
+
+                {/* BLOG TEAM */}
+
+
+                <div class="team row text-center">
+                    <div class="a fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <img class="people" src={t1} />
+                    </div>
+
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <img src={t2} />
+                    </div>
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <img src={t3} />
+                    </div>
+                    <div class="fs-1 fw-bolder mt-3 field row col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                        <img src={t4} />
+                    </div>
+                    
+                </div>
+
+
 
             </div>
             
